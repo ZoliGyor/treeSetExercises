b54831d4a577d0d455708146527a924308f49b04
Gyor <zoltan.laszlo.gyor@gmail.com>
2021-04-03 15:11:51 +0100

    Did the 9th exercise. Also, changed the names of some variables

--- Exercises.java
+++ Exercises.java
@@ -1,31 +1,40 @@
 import java.util.Iterator;
 import java.util.TreeSet;
 
-public class Exercises {
-    private TreeSet<String> set = new TreeSet<String>();
+import jdk.nashorn.api.tree.Tree;
 
-    public void exercise1(TreeSet<String> set, int number)
+public class Exercises {
+    private TreeSet<String> stringSet = new TreeSet<String>();
+    private TreeSet<Integer> intSet = new TreeSet<Integer>();
+
+    public void exercise1(TreeSet<String> stringSet, int number)
     {
-        System.out.println("1st exercise: Write a Java program to create a new tree set, add some colors (string) and print out the tree set");
+        System.out.println("1st exercise: Write a Java program to create a new tree stringSet, add some colors (string) and print out the tree stringSet");
         if(number == 1){
-            set.add("Percy");
-            set.add("Vax");
-            set.add("Grog");
-            set.add("Scanlan");
+            stringSet.add("Percy");
+            stringSet.add("Vax");
+            stringSet.add("Grog");
+            stringSet.add("Scanlan");
         }else if(number == 2){
-            set.add("Keyleth");
-            set.add("Pike");
-            set.add("Vex");
+            stringSet.add("Keyleth");
+            stringSet.add("Pike");
+            stringSet.add("Vex");
         }
+        intSet.add(5);
+        intSet.add(7);
+        intSet.add(6);
+        intSet.add(9);
+        intSet.add(1);
+        intSet.add(4);
         
-        System.out.println(set);
+        System.out.println(stringSet);
         System.out.println();
     }
 
-    public void exercise2(TreeSet<String> set)
+    public void exercise2(TreeSet<String> stringSet)
     {
-        System.out.println("2nd exercise: Write a Java program to iterate through all elements in a tree set");
-        Iterator iterator = set.iterator();
+        System.out.println("2nd exercise: Write a Java program to iterate through all elements in a tree stringSet");
+        Iterator iterator = stringSet.iterator();
         while(iterator.hasNext()){
             Object element = iterator.next();
             System.out.println(element);
@@ -33,66 +42,78 @@ public class Exercises {
         System.out.println();
     }
 
-    public void exercise3(TreeSet<String> set1, TreeSet<String> set2)
+    public void exercise3(TreeSet<String> stringSet1, TreeSet<String> stringSet2)
     {
-        System.out.println("3rd exercise: Write a Java program to add all the elements of a specified tree set to another tree set");
-        set1.addAll(set2);
-        System.out.println(set1);
+        System.out.println("3rd exercise: Write a Java program to add all the elements of a specified tree stringSet to another tree stringSet");
+        stringSet1.addAll(stringSet2);
+        System.out.println(stringSet1);
         System.out.println();
     }
 
-    public void exercise4(TreeSet<String> set)
+    public void exercise4(TreeSet<String> stringSet)
     {
-        System.out.println("4th exercise: Write a Java program to create a reverse order view of the elements contained in a given tree set");
-        System.out.println(set.descendingSet());
+        System.out.println("4th exercise: Write a Java program to create a reverse order view of the elements contained in a given tree stringSet");
+        System.out.println(stringSet.descendingSet());
         System.out.println();
     }
 
-    public void exercise5(TreeSet<String> set)
+    public void exercise5(TreeSet<String> stringSet)
     {
-        System.out.println("5th exercise: Write a Java program to get the first and last elements in a tree set");
-        System.out.println("The first elemenet of the set is: " + set.first());
-        System.out.println("The last elemenet of the set is: " + set.last());
+        System.out.println("5th exercise: Write a Java program to get the first and last elements in a tree stringSet");
+        System.out.println("The first elemenet of the stringSet is: " + stringSet.first());
+        System.out.println("The last elemenet of the stringSet is: " + stringSet.last());
         System.out.println();
     }
 
-    public void exercise6(TreeSet<String> set1, TreeSet<String> set2)
+    public void exercise6(TreeSet<String> stringSet1, TreeSet<String> stringSet2)
     {
-        System.out.println("6th exercise: Write a Java program to clone a tree set list to another tree set");
-        System.out.println("1st set: " + set1);
-        System.out.println("2nd set: " + set2);
-        set2 = (TreeSet<String>) set1.clone();
-        System.out.println("2nd set after cloning: " + set2);
+        System.out.println("6th exercise: Write a Java program to clone a tree stringSet list to another tree stringSet");
+        System.out.println("1st stringSet: " + stringSet1);
+        System.out.println("2nd stringSet: " + stringSet2);
+        stringSet2 = (TreeSet<String>) stringSet1.clone();
+        System.out.println("2nd stringSet after cloning: " + stringSet2);
         System.out.println();
     }
 
-    public void exercise7(TreeSet<String> set)
+    public void exercise7(TreeSet<String> stringSet)
     {
-        System.out.println("7th exercise: Write a Java program to get the number of elements in a tree set");
-        System.out.println(set);
-        System.out.println("The size of the set is: " + set.size());
+        System.out.println("7th exercise: Write a Java program to get the number of elements in a tree stringSet");
+        System.out.println(stringSet);
+        System.out.println("The size of the stringSet is: " + stringSet.size());
         System.out.println();
     }
 
-    public void exercise8(TreeSet<String> set1, TreeSet<String> set2)
+    public void exercise8(TreeSet<String> stringSet1, TreeSet<String> stringSet2)
     {
-        System.out.println("8th exercise: Write a Java program to compare two tree sets");
-        System.out.println("The first set: " + set1);
-        System.out.println("The second set: " + set2);
+        System.out.println("8th exercise: Write a Java program to compare two tree stringSets");
+        System.out.println("The first stringSet: " + stringSet1);
+        System.out.println("The second stringSet: " + stringSet2);
         boolean theyAreSame = true;
-        if(set1.size() != set2.size() || !set1.containsAll(set2)){
+        if(stringSet1.size() != stringSet2.size() || !stringSet1.containsAll(stringSet2)){
             theyAreSame = false;
         }
         if(theyAreSame){
-            System.out.println("The sets are the same");
+            System.out.println("The stringSets are the same");
         }else{
-            System.out.println("The sets are not the same");
+            System.out.println("The stringSets are not the same");
         }
         System.out.println();
     }
 
-    public TreeSet<String> getSet()
+    public void exercise9(TreeSet<Integer> intSet)
     {
-        return set;
+        System.out.println("9th exercise: Write a Java program to find the numbers less than 7 in a tree set");
+        System.out.println(intSet);
+        System.out.println(intSet.headSet(7));
+    }
+
+    public TreeSet<String> getStringSet()
+    {
+        return stringSet;
+    }
+
+    public TreeSet<Integer> getIntSet()
+    {
+        return intSet;
     }
 }
--- Main.java
+++ Main.java
@@ -3,15 +3,16 @@ public class Main {
         Exercises treeSet1 = new Exercises();
         Exercises treeSet2 = new Exercises();
 
-        treeSet1.exercise1(treeSet1.getSet(), 1);
-        treeSet2.exercise1(treeSet2.getSet(), 2);
+        treeSet1.exercise1(treeSet1.getStringSet(), 1);
+        treeSet2.exercise1(treeSet2.getStringSet(), 2);
 
-        treeSet1.exercise2(treeSet1.getSet());
-        treeSet1.exercise3(treeSet1.getSet(), treeSet2.getSet());
-        treeSet1.exercise4(treeSet1.getSet());
-        treeSet1.exercise5(treeSet1.getSet());
-        treeSet1.exercise6(treeSet1.getSet(), treeSet2.getSet());
-        treeSet1.exercise7(treeSet1.getSet());
-        treeSet1.exercise8(treeSet1.getSet(), treeSet2.getSet());
+        treeSet1.exercise2(treeSet1.getStringSet());
+        treeSet1.exercise3(treeSet1.getStringSet(), treeSet2.getStringSet());
+        treeSet1.exercise4(treeSet1.getStringSet());
+        treeSet1.exercise5(treeSet1.getStringSet());
+        treeSet1.exercise6(treeSet1.getStringSet(), treeSet2.getStringSet());
+        treeSet1.exercise7(treeSet1.getStringSet());
+        treeSet1.exercise8(treeSet1.getStringSet(), treeSet2.getStringSet());
+        treeSet1.exercise9(treeSet1.getIntSet());
     }
 }
