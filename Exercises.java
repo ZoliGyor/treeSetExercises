c0cde607317c7a5f16eb34941c4a8566b8857469
Gyor <zoltan.laszlo.gyor@gmail.com>
2021-04-03 14:17:38 +0100

    Did the 1st exercise

--- Exercises.java
+++ Exercises.java
@@ -1,3 +1,20 @@
+import java.util.TreeSet;
+
 public class Exercises {
-    
+    private TreeSet<String> set = new TreeSet<String>();
+
+    public void exercise1(TreeSet<String> set)
+    {
+        System.out.println("1st exercise: Write a Java program to create a new tree set, add some colors (string) and print out the tree set");
+        set.add("Percy");
+        set.add("Vax");
+        set.add("Grog");
+        System.out.println(set);
+        System.out.println();
+    }
+
+    public TreeSet<String> getSet()
+    {
+        return set;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        Exercises treeSet1 = new Exercises();
+
+        treeSet1.exercise1(treeSet1.getSet());
+    }
 }
