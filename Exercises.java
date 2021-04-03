760af15635bb13b725caece228d246994fa66317
Gyor <zoltan.laszlo.gyor@gmail.com>
2021-04-03 14:33:06 +0100

    Did the 3rd exercise and made soma changes in the 1st one

--- Exercises.java
+++ Exercises.java
@@ -4,12 +4,20 @@ import java.util.TreeSet;
 public class Exercises {
     private TreeSet<String> set = new TreeSet<String>();
 
-    public void exercise1(TreeSet<String> set)
+    public void exercise1(TreeSet<String> set, int number)
     {
         System.out.println("1st exercise: Write a Java program to create a new tree set, add some colors (string) and print out the tree set");
-        set.add("Percy");
-        set.add("Vax");
-        set.add("Grog");
+        if(number == 1){
+            set.add("Percy");
+            set.add("Vax");
+            set.add("Grog");
+            set.add("Scanlan");
+        }else if(number == 2){
+            set.add("Keyleth");
+            set.add("Pike");
+            set.add("Vex");
+        }
+        
         System.out.println(set);
         System.out.println();
     }
@@ -22,6 +30,14 @@ public class Exercises {
             Object element = iterator.next();
             System.out.println(element);
         }
+        System.out.println();
+    }
+
+    public void exercise3(TreeSet<String> set1, TreeSet<String> set2)
+    {
+        System.out.println("3rd exercise: Write a Java program to add all the elements of a specified tree set to another tree set");
+        set1.addAll(set2);
+        System.out.println(set1);
         System.out.println();
     }
 
--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         Exercises treeSet1 = new Exercises();
+        Exercises treeSet2 = new Exercises();
 
-        treeSet1.exercise1(treeSet1.getSet());
+        treeSet1.exercise1(treeSet1.getSet(), 1);
+        treeSet2.exercise1(treeSet2.getSet(), 2);
+
         treeSet1.exercise2(treeSet1.getSet());
+        treeSet1.exercise3(treeSet1.getSet(), treeSet2.getSet());
     }
 }
